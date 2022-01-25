7f74ba5306b6b22c48284e6a857dac9905b1f816
Caio_Manzini <kaiomnzn@gmail.com>
2022-01-25 01:20:12 -0300

    aqui eu usei o comando de for para rola a repetição, em seguida da soma, calculando as notas dos alunos e somando a media.

---
+++ Desafio_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nota dos alunos");
+
+            float soma = 0;
+            float notas = 0;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Digite a nota do aluno " + i + ":");
+                float nota = 0;
+                float.TryParse(Console.ReadLine(), out nota);
+                soma += nota;
+            }
+            notas = soma / 3;
+            Console.WriteLine("media é igual a: " + notas);
+            Console.ReadKey();
+        }
+    }
+}
